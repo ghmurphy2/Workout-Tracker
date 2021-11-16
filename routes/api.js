f0991e162e4542fd11adc6507e9d81a758925d35
Gavin Murphy <gavinhmurphy@gmail.com>
2021-11-15 17:08:20 -0800

    get routes in

--- routes/api.js
+++ routes/api.js
@@ -1,1 +1,28 @@
-// requires, get post
+// requires, get post rember error catch and sort
+const router = require('express').Router();
+const { workouts, exercises } = require('../models');
+const { Types } = require('mongoose');
+const date = Date.now();
+
+router
+.route('/workouts')
+// post route with error
+.post(async (req, res) => {
+    try{
+        const created = await workouts.create({date});
+        res.status(200).json(created);
+    } catch (err){
+        res.status(500).json(err)
+    }
+    })
+    // get all
+router.get('/workouts/range', async(req, res) =>{
+    try{
+        const workoutsAll = await workouts.find({}).populate('exercises')
+        res.status(200).json(workoutsAll)
+    }
+    catch (err){
+        res.status(500).json(err)
+    }
+    });
+}
